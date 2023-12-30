1de782752072f8dc72c83b1a946debfbddb734cb
Shantzformation <Tsm012@bucknell.edu>
2023-12-29 17:28:40 -0800

    Realtime Client

---
+++ GRPCClient/GreeterClient.h
@@ -0,0 +1,75 @@
+#include <grpcpp/grpcpp.h>
+#include "Proto/cpp-service-code/Messages.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientAsyncResponseReader;
+using grpc::ClientContext;
+using grpc::CompletionQueue;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
+
+class GreeterClient {
+public:
+    explicit GreeterClient(std::shared_ptr<Channel> channel)
+        : stub_(Greeter::NewStub(channel)) {}
+
+    void SayHello(const std::string& user) {
+
+        HelloRequest request;
+        request.set_name(user);
+
+        AsyncClientCall* call = new AsyncClientCall;
+
+        call->response_reader = stub_->PrepareAsyncSayHello(&call->context, request, &completetionQueue);
+
+        call->response_reader->StartCall();
+
+        call->response_reader->Finish(&call->reply, &call->status, (void*)call);
+    }
+
+    void AsyncCompleteRpc() {
+        void* got_tag;
+        bool ok = false;
+
+        while (completetionQueue.Next(&got_tag, &ok)) {
+            AsyncClientCall* call = static_cast<AsyncClientCall*>(got_tag);
+
+            GPR_ASSERT(ok);
+
+            if (call->status.ok())
+                std::cout << "Greeter received: " << call->reply.message() << std::endl;
+            else
+                std::cout << "RPC failed" << std::endl;
+
+            // Once we're complete, deallocate the call object.
+            delete call;
+        }
+    }
+
+private:
+    // struct for keeping state and data information
+    struct AsyncClientCall {
+        // Container for the data we expect from the server.
+        HelloReply reply;
+
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        ClientContext context;
+
+        // Storage for the status of the RPC upon completion.
+        Status status;
+
+        std::unique_ptr<ClientAsyncResponseReader<HelloReply>> response_reader;
+    };
+
+    // Out of the passed in Channel comes the stub, stored here, our view of the
+    // server's exposed services.
+    std::unique_ptr<Greeter::Stub> stub_;
+
+    // The producer-consumer queue we use to communicate asynchronously with the
+    // gRPC runtime.
+    CompletionQueue completetionQueue;
+};
